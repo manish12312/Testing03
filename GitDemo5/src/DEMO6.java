5289108c4ddd6c300cea5368c3b8cb3a9ca9731b
MANISH <MANISH@DESKTOP-NPV50CU>
2021-04-19 17:07:59 +0530

    Submitted By Manish

---
+++ GitDemo5/src/DEMO6.java
@@ -0,0 +1,22 @@
+
+public class DEMO6 
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println(" hello demo6");
+		
+	}
+	public void m10()
+	{
+		System.out.println(" M 10 ");
+	}
+	public void add()
+	{
+		System.out.println(" Hello ADD");
+		
+	}
+	
+	
+
+}
